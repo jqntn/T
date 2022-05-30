b2398db60b2e43f85de3e62ec3d50199b650ea1d
Julien QUENTIN <jqntn88@gmail.com>
2022-05-31 01:21:32 +0200

    update

--- I.bat
+++ I.bat
@@ -1,4 +1,4 @@
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64"
 
-del inc.txt
-powershell -command "dumpbin /exports %WinDir%\System32\user32.dll | sls '(?<=0 )[a-z].*' | foreach {ac inc.txt $_.matches.value -nonewline; ac inc.txt ' proto'}"
+del I
+powershell -command "dumpbin /exports %WinDir%\System32\user32.dll | sls '(?<=0 )[a-z].*' | foreach {ac I $_.matches.value -nonewline; ac I ' proto'}"
