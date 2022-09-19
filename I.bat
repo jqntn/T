2a307f31a4ff18c4e47cd12809034a68d5409591
Julien QUENTIN <jqntn88@gmail.com>
2022-09-19 22:44:56 +0200

    update

--- I.bat
+++ I.bat
@@ -2,4 +2,4 @@ call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\v
 
 del I
 
-powershell -command "sls -path T.txt '(?<=lib )[a-z].*(?=\.)' | foreach {dumpbin /exports %WinDir%\System32\$($_.matches.value).dll | sls '(?<=\S{1,3} \S{7}0 )\S*' | foreach {ac I $_.matches.value -nonewline; ac I ' proto'}}"
+powershell -command "sls -path T.txt '(?<=lib )[a-z].*' | foreach {dumpbin /exports %WinDir%\System32\$($_.matches.value).dll | sls '(?<=\S{1,3} \S{7}0 )\S*' | foreach {ac I $_.matches.value -nonewline; ac I ' proto'}}"
